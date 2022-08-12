28b342d60035152311505b016530525d325afa2e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-12 17:19:24 -0500

    im an idiot

---
+++ .next/static/1b2bOUsTRPOOBh-B4lY3H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4f29ca251d4ff3a1.css","static/chunks/pages/index-3aee575372444ccf.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
