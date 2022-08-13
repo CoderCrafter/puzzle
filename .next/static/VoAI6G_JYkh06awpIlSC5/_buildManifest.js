44e1a22dc08b99ebd8e11a1cbf5dc9b8ce2a10a7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-13 16:13:52 -0500

    build it so it works better or something

---
+++ .next/static/VoAI6G_JYkh06awpIlSC5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4f29ca251d4ff3a1.css","static/chunks/pages/index-027d1eb34cf437fd.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
