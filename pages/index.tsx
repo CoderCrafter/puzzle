299456fcbf3d78ac20431d2a0e62cef8831f46f6
BuildTools <unconfigured@null.spigotmc.org>
2022-08-16 19:25:28 -0500

    time change for ease

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Countdown from '../comps/Countdown.tsx'
 console.log(process.env)
 
 const Home = (props) => {
-  let date = new Date("2022-08-17T11:30:00.000-06:00")
+  let date = new Date("2022-08-19T11:30:00.000-07:00")
 
   let cypher = "test"
   let test = new Date("8/12/22")
